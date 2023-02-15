0a29864dadf50631c2d9326344564983aeba8ab7
Jordin Marshall <jordin.marshall@technologyadvice.com>
2023-02-15 13:56:48 -0500

    updated deploy to account for dist getting re-created on every build

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:jordin-marshall/ta-documentation.git docs
+git push -f git@github.com:jordin-marshall/ta-documentation.git master:docs
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
